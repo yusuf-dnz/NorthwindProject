61c6352631d9a9afaf814135dc99e1c2b39f24ce
Yusuf Deniz <joseph__sea@hotmail.com>
2023-12-30 00:53:24 +0300

    .

--- Core/Utilities/Interceptors/MethodInterception.cs
+++ Core/Utilities/Interceptors/MethodInterception.cs
@@ -1,24 +1,7 @@
 ﻿using Castle.DynamicProxy;
-using Microsoft.EntityFrameworkCore.Diagnostics;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Core.Utilities.Interceptors
 {
-    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true, Inherited = true)]
-    public abstract class MethodInterceptionBaseAttribute : Attribute, IInterceptor
-    {
-        public int Priority { get; set; }
-
-        public virtual void Intercept(IInvocation invocation)
-        {
-
-        }
-    }
-
     public abstract class MethodInterception : MethodInterceptionBaseAttribute
     {
         protected virtual void OnBefore(IInvocation invocation) { }
