4f101e0ba205545be8abd4b3570e49a7747e269b
Yusuf Deniz <joseph__sea@hotmail.com>
2023-12-30 00:08:12 +0300

    .

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,15 +1,21 @@
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
 using Business.Abstract;
 using Business.Concrete;
+using Business.DependencyResolvers.AutoFac;
 using DataAccess.Abstract;
 using DataAccess.Concrete.EntityFramework;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
+builder.Host.ConfigureContainer<ContainerBuilder>(
+    builder => builder.RegisterModule(new AutoFacBusinessModule()));
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddSingleton<IProductService, ProductManager>();
-builder.Services.AddSingleton<IProductDal, EfProductDal>();
+//builder.Services.AddSingleton<IProductService, ProductManager>();
+//builder.Services.AddSingleton<IProductDal, EfProductDal>();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
